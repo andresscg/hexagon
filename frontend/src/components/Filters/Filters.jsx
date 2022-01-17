30b4201d178cc2b33499812458bfa6180c31f41e
bk-sin <beckasin.mc@gmail.com>
2022-01-16 23:25:58 -0300

    hook de dimensiones y barra de busqueda

--- frontend/src/components/Filters/Filters.jsx
+++ frontend/src/components/Filters/Filters.jsx
@@ -5,19 +5,35 @@ import "../../styles/Filters.css"
 import "../../styles/ShopContainer.css"
 import SideBarDrawer from "./SideBarDrawer"
 import Productos from "../../pages/Productos"
+import {FormControl} from "react-bootstrap"
+import useWindowDimensions from "../../hooks/useWindowDimensions"
+import SideBarFilter from "./SideBarFilter"
 
 function Filters(props) {
+  const {height, width} = useWindowDimensions()
+  console.log(width)
   useEffect(() => {
     props.fetchearProductos()
   }, [])
 
   return (
-    <div className="shop__container" style={{height: "100vh"}}>
-      <div className="shop__side-bar">
-        <SideBarDrawer productos={props.productos} filtro={props.filtro} />
-      </div>
-      <div className="shop__content">
-        <Productos />
+    <div className="shop__main">
+      <FormControl
+        onChange={(e) => props.filtro(e.target.value.toLowerCase().trim())}
+        placeholder="FIND YOUR PRODUCT"
+        aria-describedby="inputGroup-sizing-sm"
+      />
+      <div className="shop__container" style={{height: "100vh"}}>
+        <div className="shop__side-bar">
+          {width >= 1300 ? (
+            <SideBarFilter productos={props.productos} />
+          ) : (
+            <SideBarDrawer productos={props.productos} filtro={props.filtro} />
+          )}
+        </div>
+        <div className="shop__content">
+          <Productos />
+        </div>
       </div>
     </div>
   )
