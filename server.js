ecf3a7932221daa2263e9396790991804025551f
Emiliano Alegre <beckasin.mc@gmail.com>
2022-01-18 02:07:29 -0300

    http2

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config()
 require("./config/database")
 require("./config/passport")
+
+const fs = require("fs")
+const spdy = require("spdy")
 const express = require("express")
 const path = require("path")
 const cors = require("cors")
@@ -10,11 +13,16 @@ const compression = require("compression")
 const app = express()
 const PORT = process.env.PORT || 4000
 
+const options = {
+  key: fs.readFileSync(__dirname + "/server.key"),
+  cert: fs.readFileSync(__dirname + "/server.crt"),
+}
 app.use(cors())
 app.use(express.json())
 app.use(passport.initialize())
 app.use("/api", router)
 app.use(compression())
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"))
   app.get("*", (req, res) => {
@@ -22,4 +30,13 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-app.listen(PORT, () => console.log("App listening on port " + PORT))
+spdy.createServer(options, app).listen(PORT, (error) => {
+  if (error) {
+    console.error(error)
+    return process.exit(1)
+  } else {
+    console.log("Listening on port: " + PORT + ".")
+  }
+})
+
+/* app.listen(PORT, () => console.log("App listening on port " + PORT)) */
