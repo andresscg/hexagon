c0eec346cac50396382d28c38b22b4933b76c678
Emiliano Alegre <beckasin.mc@gmail.com>
2022-01-19 11:31:27 -0300

    fix navbar img

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,107 +25,109 @@ const Navbar = (props) => {
             <p className="nav__title">HEXAGON</p>
             <p className="nav__subtitle">TECHSTORE</p>
           </div>
-          </div>
-          <div className="nav__menu--navigation">
-            <button className="custom-btn btn-3">
-              <span>
-                <Link to="/" className="nav__menu--item text-light">
-                  Home
-                </Link>
-              </span>
-            </button>
-            <button className="custom-btn btn-3">
-              <span>
-                <Link to="/shop" className="nav__menu--item text-light">
-                  Shop
-                </Link>
-              </span>
-            </button>
-            <button className="custom-btn btn-3">
-              <span>
-                <Link to="/contact" className="nav__menu--item text-light">
-                  Contact
-                </Link>
-              </span>
-            </button>
-            <div className="nav__menu--sign">
-              {!props.isLoading && props.isAuth ? (
-                <>
-                  <button
-                    onClick={() => props.logout()}
-                    className="custom-btn btn-3"
-                  >
-                      <span>
-                        Log Out
-                      </span>
-                  </button>
-                  <div className="user__info">
-                    <div
-                      style={{backgroundImage: `url(${props.user?.photo})`}}
-                      className="nav__menu__photo"
-                    />
-                    <p className="user__name">{props.user?.firstName}</p>
-                  </div>
-                </>
-              ) : (
+        </div>
+        <div className="nav__menu--navigation">
+          <button className="custom-btn btn-3">
+            <span>
+              <Link to="/" className="nav__menu--item text-light">
+                Home
+              </Link>
+            </span>
+          </button>
+          <button className="custom-btn btn-3">
+            <span>
+              <Link to="/shop" className="nav__menu--item text-light">
+                Shop
+              </Link>
+            </span>
+          </button>
+          <button className="custom-btn btn-3">
+            <span>
+              <Link to="/contact" className="nav__menu--item text-light">
+                Contact
+              </Link>
+            </span>
+          </button>
+          <div className="nav__menu--sign">
+            {!props.isLoading && props.isAuth ? (
+              <>
                 <button
-                  onClick={() => props.showCloseModal()}
+                  onClick={() => props.logout()}
                   className="custom-btn btn-3"
                 >
-                    <span>
-                  Login/Register
-                    </span>
+                  <span>Log Out</span>
                 </button>
-              )}
-              {!props.isLoading && props.isAuth && props.user?.admin && (
-                <Link to={"/admin"}>Admin</Link>
-              )}
-            </div>
+                <div className="user__info">
+                  <div
+                    style={{
+                      backgroundImage: `url(${
+                        props.user?.google
+                          ? props.user?.photo
+                          : "http://localhost:4000/" + props.user?.photo
+                      })`,
+                    }}
+                    className="nav__menu__photo"
+                  />
+                  <p className="user__name">{props.user?.firstName}</p>
+                </div>
+              </>
+            ) : (
+              <button
+                onClick={() => props.showCloseModal()}
+                className="custom-btn btn-3"
+              >
+                <span>Login/Register</span>
+              </button>
+            )}
+            {!props.isLoading && props.isAuth && props.user?.admin && (
+              <Link to={"/admin"}>Admin</Link>
+            )}
           </div>
-          <Nav className="cart-fixed">
-            <Dropdown>
-              <Dropdown.Toggle>
-                  <FaShoppingCart color="white" fontSize="25px" />
-                  <Badge>{totalItems}</Badge>
-              </Dropdown.Toggle>
-              <Dropdown.Menu style={{minWidth: 370}} className="cart__dropdown">
-                {totalItems ? (
-                  <>
-                    {items.map((prod) => (
-                      <span className="cartItem" key={prod.id}>
-                        <img
-                          src={prod.image}
-                          className="cartItemImg"
-                          alt={prod.product}
-                          width={100}
-                        />
+        </div>
+        <Nav className="cart-fixed">
+          <Dropdown>
+            <Dropdown.Toggle>
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{totalItems}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{minWidth: 370}} className="cart__dropdown">
+              {totalItems ? (
+                <>
+                  {items.map((prod) => (
+                    <span className="cartItem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.product}
+                        width={100}
+                      />
 
-                        <div className="cartItemDetail" style={{gap: 10}}>
-                          <span>{prod.product}</span>
-                          <span>${prod.price}</span>
-                          <span>{prod.quantity}</span>
-                        </div>
-                        <AiFillDelete
-                          fontSize="20px"
-                          style={{cursor: "pointer"}}
-                          onClick={() => {
-                            removeItem(prod.id)
-                          }}
-                        />
-                      </span>
-                    ))}
-                    <Link to="/cart">
-                      <Button style={{width: "95%", margin: "0 10px"}}>
-                        Go To Cart
-                      </Button>
-                    </Link>
-                  </>
-                ) : (
-                  <span style={{padding: 10}}>Cart is Empty</span>
-                )}
-              </Dropdown.Menu>
-            </Dropdown>
-          </Nav>
+                      <div className="cartItemDetail" style={{gap: 10}}>
+                        <span>{prod.product}</span>
+                        <span>${prod.price}</span>
+                        <span>{prod.quantity}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{cursor: "pointer"}}
+                        onClick={() => {
+                          removeItem(prod.id)
+                        }}
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{width: "95%", margin: "0 10px"}}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{padding: 10}}>Cart is Empty</span>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
       </div>
     </>
   )
