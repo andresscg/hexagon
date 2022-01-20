cfa3efd09f72a09c6454f3a6d0c6724ccd7f1de7
silc22 <you@example.com>
2022-01-19 21:28:28 -0300

    new call to action hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -12,12 +12,17 @@ const Hero = () => {
         <p className="heroInfo__subtitle">
           Thanks to Hexagon, you can find yourself a brand new phone or give to your current one new looks and accessories.
         </p>
-        <button className="custom-btn btn-3">
-          <span>
-            <Link to="/shop" className="nav__menu--item text-light">
-              Go Shopping!
-            </Link>
-          </span>
+        <button class='button__2' data-text='GO SHOPPING!'>
+          <span>E</span>
+          <span>N</span>
+          <span>J</span>
+          <span>O</span>
+          <span>Y</span>
+          <span></span>
+          <span>I</span>
+          <span>T</span>
+          <span> </span>
+          <span>😉</span>
         </button>
       </div>
       <img src="/assets/hero-img.svg" alt="Phone Image" className='heroImg'/>
